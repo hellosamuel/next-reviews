e1f7d11bd022caa35ef94525c087d4136f653c67
Samuel Lee <samuel.lee@enigmo.co.jp>
2023-08-23 17:25:18 +0900

    section3 done

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import type { ReactNode } from 'react'
+
+interface LayoutProps {
+  children: ReactNode
+}
+
+export default function RootLayout({ children }: LayoutProps) {
+  return (
+    <html lang="en">
+      <body>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/reviews">Reviews</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {children}
+        <footer>
+          Game data and images courtesy of{' '}
+          <a href="https://rawg.io" target="_blank">
+            RAWG
+          </a>
+        </footer>
+      </body>
+    </html>
+  )
+}
---
+++ app/reviews/stardew-valley/page.tsx
@@ -0,0 +1,8 @@
+export default function StarDewValleyPage() {
+  return (
+    <>
+      <h1>Stardew Valley</h1>
+      <p>This will be the review for Stardew Valley.</p>
+    </>
+  )
+}
