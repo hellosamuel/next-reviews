b306d0daa74065ed26a85099362570a57f4e5b42
Samuel Lee <samuel.lee@enigmo.co.jp>
2023-11-01 15:31:24 +0900

    section4 done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
-import Link from 'next/link'
 import type { ReactNode } from 'react'
+import './globals.css'
+
+import NavBar from '@/components/NavBar'
 
 interface LayoutProps {
   children: ReactNode
@@ -8,26 +10,14 @@ interface LayoutProps {
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-orange-50 flex flex-col px-4 py-2 min-h-screen">
         <header>
-          <nav>
-            <ul>
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="/reviews">Reviews</Link>
-              </li>
-              <li>
-                <Link href="/about">About</Link>
-              </li>
-            </ul>
-          </nav>
+          <NavBar />
         </header>
-        {children}
-        <footer>
+        <main className="grow py-3">{children}</main>
+        <footer className="border-t py-3 text-center text-xs">
           Game data and images courtesy of{' '}
-          <a href="https://rawg.io" target="_blank">
+          <a href="https://rawg.io" target="_blank" className="text-orange-800 hover:underline">
             RAWG
           </a>
         </footer>
