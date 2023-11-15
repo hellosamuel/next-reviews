33940677715c757662e9fdd49a6d8781f761c96c
hellosamuel <samuelis419@gmail.com>
2023-11-15 22:28:59 +0900

    section6 done

---
+++ app/fonts.ts
@@ -0,0 +1,5 @@
+import { Exo_2, Orbitron } from 'next/font/google'
+
+export const exo_2 = Exo_2({ subsets: ['latin'], variable: '--font-exo_2' })
+
+export const orbitron = Orbitron({ subsets: ['latin'], variable: '--font-orbitron' })
---
+++ lib/reviews.ts
@@ -0,0 +1,46 @@
+import { readdir, readFile } from 'node:fs/promises'
+import matter from 'gray-matter'
+import { marked } from 'marked'
+
+export interface Review {
+  slug: string
+  title: string
+  date: string
+  image: string
+  body: string
+}
+
+export async function getFeaturedReview(): Promise<Review> {
+  const reviews = await getReviews()
+  return reviews[0]
+}
+
+export async function getReview(slug: string): Promise<Review> {
+  const text = await readFile(`./content/reviews/${slug}.md`, 'utf8')
+  const {
+    content,
+    data: { date, title, image }
+  } = matter(text)
+  const body = marked(content)
+
+  return { slug, date, title, image, body }
+}
+
+export async function getReviews(): Promise<Review[]> {
+  const slugs = await getSlugs()
+  const reviews: Review[] = []
+  for (const slug of slugs) {
+    const review = await getReview(slug)
+    reviews.push(review)
+  }
+
+  reviews.sort((a, b) => b.date.localeCompare(a.date))
+  return reviews
+}
+
+export async function getSlugs() {
+  const files = await readdir('./content/reviews')
+  const slugs = files.filter(file => file.endsWith('.md')).map(file => file.slice(0, -'.md'.length))
+
+  return slugs
+}
