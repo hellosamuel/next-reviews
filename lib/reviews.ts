edfdd836760a8045c1a1ffd5837aa755b9ce3a16
hellosamuel <samuelis419@gmail.com>
2023-12-27 11:07:03 +0900

    section15 done (Deploying With External Services)

--- lib/reviews.ts
+++ lib/reviews.ts
@@ -5,7 +5,7 @@ import qs from 'qs'
 
 export const CACHE_TAG_REVIEWS = 'reviews'
 
-const CMS_URL = 'http://localhost:1337'
+const CMS_URL = process.env.CMS_URL
 
 export interface CmsItem {
   id: number
@@ -104,6 +104,6 @@ function toReview(item: CmsItem): Review {
     title: attributes.title,
     subtitle: attributes.subtitle,
     date: attributes.publishedAt.slice(0, 'yyyy-mm-dd'.length),
-    image: CMS_URL + attributes.image.data.attributes.url
+    image: new URL(attributes.image.data.attributes.url, CMS_URL).href
   }
 }
