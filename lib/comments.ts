ba261c2f3c9bd99c6e9d31565d3410c807b446a1
hellosamuel <samuelis419@gmail.com>
2024-02-06 23:29:19 +0900

    section17 done (Server Actions)

--- lib/comments.ts
+++ lib/comments.ts
@@ -12,6 +12,7 @@ export async function createComment({ slug, user, message }: CreateCommentData) 
 
 export async function getComments(slug: string) {
   return await db.comment.findMany({
-    where: { slug }
+    where: { slug },
+    orderBy: { postedAt: 'desc' },
   })
 }
